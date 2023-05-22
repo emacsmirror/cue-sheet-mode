29432da85e17e87adec760cdecdcc01aa9cc0202
Peter Hoeg <peter@hoeg.com>
2023-05-22 11:37:38 +0800

    fix font-locking

--- cue-sheet-mode.el
+++ cue-sheet-mode.el
@@ -1,4 +1,4 @@
-;;; cue-sheet-mode.el --- Major mode for editing CUE files
+;;; cue-sheet-mode.el --- Major mode for editing CUE sheet files
 
 ;; Copyright © 2017, by Peter Hoeg
 
@@ -6,7 +6,7 @@
 ;; Version: 1.0.0
 ;; Created: 2017
 ;; Keywords: languages
-;; Homepage: https://hoeg.com
+;; Homepage: https://github.com/peterhoeg/cue-sheet-mode
 ;; Package-Requires: ((emacs "24.3"))
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 ;;
@@ -22,23 +22,20 @@
 (require 'rx)
 
 ;;;###autoload
-(define-derived-mode cue-sheet-mode conf-mode "CUE mode"
-  "Major mode for editing CUE files."
+(define-derived-mode cue-sheet-mode conf-mode "CUESheet"
+  "Major mode for editing CUE sheet files."
 
   (setq-local comment-start "REM"
               comment-end ""
-              font-lock-defaults `(((,(rx (or "REM")) 'font-lock-comment-delimiter-face)
-                                    (,(rx (or "CATALOG" "COMPOSER" "FLAGS" "INDEX" "ISRC" "PERFORMER" "TITLE")) . 'font-lock-keyword-face)
-                                    (,(rx (or "COMMENT" "DATE" "DISCID" "GENRE")) . 'font-lock-builtin-face)
-                                    (,(rx (or "AUDIO")) . 'font-lock-function-name-face)
-                                    (,(rx (or "BINARY" "FILE" "MP3" "TRACK" "WAVE")) . 'font-lock-type-face)))
+              font-lock-defaults `(((,(rx bol (or "REM")) . 'font-lock-comment-face)
+                                    (,(rx (or "CATALOG" "CDTEXTFILE" "COMMENT" "COMPOSER" "DATE" "DISCID" "FILE" "FLAGS" "GENRE" "INDEX" "ISRC" "PERFORMER" "POSTGAP" "PREGAP" "SONGWRITER" "TITLE" "TRACK")) . 'font-lock-keyword-face)
+                                    (,(rx (or "AUDIO" "BINARY" "CDI/2336" "CDI/2352" "CDG" "MODE1/2048" "MODE1/2352" "MODE2/2336" "MODE2/2352" "MP3" "WAVE")) . 'font-lock-type-face)))
               imenu-generic-expression `(("Track" ,(rx bol (zero-or-more space) "TRACK" (one-or-more space) (group (one-or-more digit)) space "AUDIO" eol) 1)
                                          ("File"  ,(rx bol "FILE" (one-or-more space) "\"" (group (one-or-more not-newline)) "\"" space "WAVE" eol) 1 ))))
 
 ;;;###autoload
-(add-to-list 'auto-mode-alist '("\\.cue\\'" . cue-sheet-mode))
+(add-to-list 'auto-mode-alist `(,(rx ".cue" eos) . cue-sheet-mode))
 
-;; add the mode to the `features' list
 (provide 'cue-sheet-mode)
 
 ;;; cue-sheet-mode.el ends here
